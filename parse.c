4c689141cb912183007357ad052342e963e071a1
Dale Weiler <weilercdale@gmail.com>
2014-04-28 12:20:26 -0400

    Finish the rest of the constant folder

--- parse.c
+++ parse.c
@@ -387,6 +387,36 @@ static ast_t *parse_expression_last(parse_t *parse, ast_t *lhs, int minprec) {
                 case LEX_TOKEN_DIV:
                     lhs->number /= rhs->number;
                     return lhs;
+                case LEX_TOKEN_MOD:
+                    lhs->number = (uint32_t)lhs->number % (uint32_t)rhs->number;
+                    return lhs;
+                case LEX_TOKEN_BITAND:
+                    lhs->number = (uint32_t)lhs->number & (uint32_t)rhs->number;
+                    return lhs;
+                case LEX_TOKEN_BITOR:
+                    lhs->number = (uint32_t)lhs->number | (uint32_t)rhs->number;
+                    return lhs;
+                case LEX_TOKEN_LESS:
+                    lhs->number = !!(lhs->number < rhs->number);
+                    return lhs;
+                case LEX_TOKEN_GREATER:
+                    lhs->number = !!(lhs->number > rhs->number);
+                    return lhs;
+                case LEX_TOKEN_LEQUAL:
+                    lhs->number = !!(lhs->number <= rhs->number);
+                    return lhs;
+                case LEX_TOKEN_GEQUAL:
+                    lhs->number = !!(lhs->number >= rhs->number);
+                    return lhs;
+                case LEX_TOKEN_EQUAL:
+                    lhs->number = lhs->number == rhs->number;
+                    return lhs;
+                case LEX_TOKEN_NEQUAL:
+                    lhs->number = lhs->number != rhs->number;
+                    return lhs;
+                case LEX_TOKEN_NOT:
+                    lhs->number = !lhs->number;
+                    return lhs;
                 default:
                     break;
             }
